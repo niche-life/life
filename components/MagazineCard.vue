d0b107af6c4929554d232ff7882ebb0031043cef
hayate.tanaka <tanattyokana@gmail.com>
2020-03-07 18:33:00 +0900

    add: faq and series pages

---
+++ components/MagazineCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="MagazineCard">
+    <v-card>
+      <v-card-title>
+        {{ items.series }}
+        <a :href="items.url">[PDF]</a>
+      </v-card-title>
+      <v-card-text>
+        <v-row justify="space-between">
+          <v-col cols="auto">
+            <v-img :src="items.image" max-width="300" />
+          </v-col>
+          <v-col class="magazine-text">
+            <time>{{ items.publishedDate }}</time>刊行
+            <span>{{ items.description }}</span>
+            <div v-for="(article, i) in items.article" :key="i">
+              <a :href="article.url">{{ article.title }}</a>
+            </div>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.MagazineCard {
+  padding: 10px;
+}
+
+.magazine-text {
+  padding: 20px;
+}
+</style>
---
+++ pages/series.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="About">
+    <h2>巻号一覧</h2>
+    
+    <div v-for="(magazine, i) in magazineData" :key="i">
+      <MagazineCard :items=magazine />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import MagazineCard from '@/components/MagazineCard.vue'
+import Magazine from '@/data/magazineData.json'
+
+export default {
+  components: {
+    MagazineCard,
+  },
+  data() {
+    return {
+      magazineData: Magazine.magazineData,
+    } 
+  }
+}
+</script>
