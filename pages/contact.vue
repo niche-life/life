c4f4c3b3bbe5ac631ffddb8b983c1871757ff88b
hayate.tanaka <tanattyokana@gmail.com>
2020-04-19 16:27:52 +0900

    軽微な修正

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,9 +2,9 @@
   <div class="Contact">
     <TextCard title="メールでのお問い合わせ">
       <p>
-        メールでのお問い合わせは次のアドレスからお願いします。<br />
-        <a href="mailto:info@niche-life.com">info@niche-life.com</a>
+        メールでのお問い合わせは次のアドレスからお願いします。
       </p>
+      <p><a href="mailto:info@niche-life.com">info@niche-life.com</a></p>
       <p>
         お問い合わせ内容によっては、数日以上のお時間をいただく場合やお答えできない場合がございます。また、回答の時間指定はお受けできませんのであらかじめご了承ください。
       </p>
